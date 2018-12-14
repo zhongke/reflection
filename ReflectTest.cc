13486d891a4f87462f951cb764a3e6ed7a9b4b06
darwin-xu <xu.dai.wen@126.com>
2018-12-14 22:30:53 +0800

    add real pot and pod.

--- ReflectTest.cc
+++ ReflectTest.cc
@@ -1,26 +1,26 @@
 #include "gmock/gmock.h"
 #include "SubscriberReflect.hh"
+#include "SubscriberPot.hh"
+#include "SubscriberPod.hh"
 
 using namespace testing;
 using namespace std;
 
-
-
-class ReflectUtil: public Test
+class ReflectUtil : public Test
 {
-public:
-        SubscriberReflect reflect;
+     public:
+       SubscriberPot pot;
+       SubscriberPod pod;
+       SubscriberReflect reflect;
 
-        ReflectUtil() : reflect(nullptr, nullptr)
-        {
-        }
+       ReflectUtil() : reflect(&pot, &pod)
+       {
+       }
 };
 
 TEST_F(ReflectUtil, getOneFilter)
 {
-       map<string, string> condition {
-              {"eventTrigger", "200"}
-       };
+       map<string, string> condition{
+           {"eventTrigger", "200"}};
        reflect.getPodByCondition(condition);
 }
-
